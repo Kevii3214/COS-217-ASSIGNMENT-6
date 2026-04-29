a39d52272f450741e8ac6c1e737644e76dc854f0
Kevvy <25ktran8@gmail.com>
2026-04-29 19:00:48 -0400

    A+ bl

--- miniassembler.c
+++ miniassembler.c
@@ -115,3 +115,21 @@ unsigned int MiniAssembler_b(unsigned long ulAddr,
    return uiInstr;
 
 }
+
+/*------------------------------------------------------------------*/
+
+unsigned int MiniAssembler_bl(unsigned long ulAddr,
+   unsigned long ulAddrOfThisInstr)
+{
+   unsigned int uiInstr;
+   unsigned int uiDisp;
+
+   /* base instruction: branch with link */
+   uiInstr = 0x94000000;
+
+   /* displacement in words inserted into bits 0-25 */
+   uiDisp = (unsigned int)((ulAddr - ulAddrOfThisInstr) / 4);
+   setField(uiDisp, 0, &uiInstr, 0, 26);
+
+   return uiInstr;
+}
